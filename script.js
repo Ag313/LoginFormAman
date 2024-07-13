af965db7d6634171184f675c4557a75a13e484d2
Ag313 <ag88969@gmail.com>
2024-07-13 16:51:09 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 
-const container= document.getElementById('container')
-const registeBtn= document.getElementById('register')
-const logintBtn= document.getElementById('login')
+const container= document.getElementById('container');
+const registeBtn= document.getElementById('register');
+const logintBtn= document.getElementById('login');
 
-registeBtn.addEventListener('click',()=>{
-container.classList.add('active')
-});
+
+
+// registeBtn.addEventListener('click',()=>{
+// container.classList.add('active')
+// });
 logintBtn.addEventListener('click',()=>{ 
     container.classList.remove('active')
 });
+
+function saveData(){
+    let Name, Email, Password;
+    Name = document.querySelector('#email').value;
+    Email = document.querySelector('#name')
+    Password = document.querySelector('#password') 
+
+
+console.log('Name','Email','Password')
+
+
+
+}
 
 
